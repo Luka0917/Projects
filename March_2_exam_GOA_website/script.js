76d0b3b781ccea8a5d04f5a58dfc52318ffb69b6
Luka0917 <tatuashvililuka@gmail.com>
2025-02-24 02:09:46 +0400

    commit

--- March_2_exam_GOA_website/script.js
+++ March_2_exam_GOA_website/script.js
@@ -1,9 +1,18 @@
-console.log(window.innerWidth);
-console.log(window.innerHeight);
-
 window.addEventListener('load', () => {
     if(localStorage.length > 0){
         document.getElementById('form-div').remove();
+        let loginDiv = document.getElementById('login-div');
+        loginDiv.id = 'nothing';
+        loginDiv.textContent = '';
+
+        const profile = document.createElement('div');
+        profile.innerHTML = '<i class="fa-solid fa-user"></i>';
+        profile.id = 'profile';
+        const name = document.createElement('p');
+        name.textContent = localStorage.getItem('user-name');
+        name.id = 'name';
+        document.getElementById('user').appendChild(profile);
+        document.getElementById('user').appendChild(name);
     };
 });
 
@@ -11,8 +20,7 @@ const userName = document.getElementById('user-name');
 const email = document.getElementById('email');
 const pass = document.getElementById('password');
 
-document.querySelector('form').addEventListener('submit', e => {
-    e.preventDefault();
+function registerCheck(){
     let haveNumber = false;
     let haveSymbol = false;
     const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '{', '}', '[', ']', '\\', '|', ';', ':', "'", '"', '<', '>', ',', '.', '/', '?', '~', '`'];
@@ -30,30 +38,56 @@ document.querySelector('form').addEventListener('submit', e => {
             localStorage.setItem('email', email.value);
             localStorage.setItem('pass', pass.value);
             document.getElementById('form-div').remove();
+
+            const profile = document.createElement('div');
+            profile.innerHTML = '<i class="fa-solid fa-user"></i>';
+            profile.id = 'profile';
+            const name = document.createElement('p');
+            name.textContent = localStorage.getItem('user-name');
+            name.id = 'name';
+            document.getElementById('user').appendChild(profile);
+            document.getElementById('user').appendChild(name);
         }else{
             alert('Password must contain at least one number and one symbol.');
         }
     }else{
         alert('Invalid Password!');
     };
-});
+    location.reload();
+}
+document.querySelector('form').addEventListener('submit', e => {e.preventDefault(); registerCheck()});
 
-function loginClicked(){
-    document.getElementById('form-div').innerHTML = `
-        <p id="login">Login</p>
-        <form>
-            <label for="user-name" id="user-label">User name</label>
-            <input type="text" id="user-name" required>
+function loginCheck(){
+    const loginDiv = document.getElementById('login-div');
+    const formDiv = document.getElementById('form-div');
+    if(loginDiv.textContent === 'Login'){
+        formDiv.innerHTML = `
+            <p id="login">Login</p>
+            <form>
+                <label for="user-name" id="user-label">User name</label>
+                <input type="text" id="user-name" required>
     
-            <label for="password" id="password-label">Password</label>
-            <input type="text" id="password" required>
+                <label for="password" id="password-label">Password</label>
+                <input type="text" id="password" required>
     
-            <button id="register">Register</button>
-        </form>
-        <p id="register-p">Don't Have An Account? <b id="acc-reg-b">Register</b></p>
-    `;
-    document.getElementById('acc-reg-b').addEventListener('click', () => {
-        document.getElementById('form-div').innerHTML = `
+                <button id="register">Register</button>
+            </form>
+            <p id="register-p">Don't Have An Account? <b id="acc-reg-b">Register</b></p>
+        `;
+        loginDiv.textContent = 'Register';
+        document.getElementById('register').textContent = 'Login';
+        document.getElementById('acc-reg-b').addEventListener('click', () => {loginCheck()});
+        loginDiv.addEventListener('click', () => {loginCheck()});
+
+        document.getElementById('register').addEventListener('click', e => {
+            e.preventDefault();
+            let errP = document.createElement('p');
+            errP.id = 'err';
+            errP.textContent = 'You Dont Have An Account!';
+            formDiv.appendChild(errP);
+        });
+    }else{
+        formDiv.innerHTML = `
             <p id="reg">Register</p>
                 <form>
                     <label for="user-name" id="user-label">User name</label>
@@ -69,10 +103,13 @@ function loginClicked(){
                 </form>
             <p id="login-p">Already Have An Account? <b id="acc-b">Login</b></p>
         `;
-        document.getElementById('acc-b').addEventListener('click', () => {loginClicked()});
-    });
+        loginDiv.textContent = 'Login';
+        document.getElementById('acc-b').addEventListener('click', () => {loginCheck()});
+        loginDiv.addEventListener('click', () => {loginCheck()});
+    };
+    document.getElementById('login-div').addEventListener('click', () => {loginCheck()});
 };
-document.getElementById('acc-b').addEventListener('click', () => {loginClicked()});
-document.getElementById('login-div').addEventListener('click', () => {loginClicked()});
+document.getElementById('login-div').addEventListener('click', () => {loginCheck()});
+document.getElementById('acc-b').addEventListener('click', () => {loginCheck()});
 
-// localStorage.clear()
+// localStorage.clear();
