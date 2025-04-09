17f2fab10cc308492e2d920519d81b3c831f2061
Luka0917 <tatuashvililuka@gmail.com>
2025-04-10 00:24:59 +0400

    commit

--- My_projects/Sport_clothing_shop/script.js
+++ My_projects/Sport_clothing_shop/script.js
@@ -15,3 +15,39 @@ for(let i = 0; i < optionsDivs.length; i++){
         discover[i].style.opacity = '0';
     });
 };
+
+let solid = '<i class="fa-solid fa-heart"></i>';
+let regular = '<i class="fa-regular fa-heart"></i>';
+const heart = document.getElementsByClassName('heart');
+for(let x = 0; x < heart.length; x++){
+    heart[x].addEventListener('click', () => {
+        if(heart[x].innerHTML === regular){
+            heart[x].innerHTML = solid;
+            heart[x].style.color = '#fe0102';
+        }else{
+            heart[x].innerHTML = regular;
+            heart[x].style.color = '#000';
+        };
+    });
+    heart[x].addEventListener('mouseenter', () => {
+        if(heart[x].innerHTML !== solid) {
+            heart[x].style.color = '#fe0102';
+        };
+    });
+    heart[x].addEventListener('mouseleave', () => {
+        if(heart[x].innerHTML !== solid) {
+            heart[x].style.color = '#000';
+        };
+    });
+};
+
+const cartP = document.getElementById('cart-p');
+const productCart = document.getElementsByClassName('product-cart');
+let products = 0;
+
+for(let j = 0; j < productCart.length; j++){
+    productCart[j].addEventListener('click', () => {
+        products++;
+        cartP.textContent = `Cart(${products})`;
+    });
+};
