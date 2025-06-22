24d554f1d3e615965b8e34e6b242356d4a91e952
Luka0917 <tatuashvililuka@gmail.com>
2025-06-22 13:47:45 +0400

    commit

--- My_projects/Coockie_clicker_2.0/script.js
+++ My_projects/Coockie_clicker_2.0/script.js
@@ -2,6 +2,7 @@ const coockieBtn = document.getElementById('coockie-btn');
 const shopDiv = document.getElementById('shop');
 const cartBtn = document.getElementById('cart');
 const xBtn = document.getElementById('x');
+
 const coockiesP = document.getElementById('coockies-p');
 const goldP = document.getElementById('gold-p');
 
@@ -16,6 +17,21 @@ const betterMinesCountP = document.getElementById('better-mines-count');
 const doubleCoockiesBtn = document.getElementById('double-coockies-btn');
 const doubleCoockiesPriceP = document.getElementById('double-coockies-price');
 const doubleCoockiesCountP = document.getElementById('double-coockies-count');
+
+const bakerBtn = document.getElementById('baker-btn');
+const bakerPriceP = document.getElementById('baker-price');
+const bakerCountP = document.getElementById('baker-count');
+
+const steveBtn = document.getElementById('steve-btn');
+const stevePriceP = document.getElementById('steve-price');
+const steveCountP = document.getElementById('steve-count');
+
+const restartBtn = document.getElementById('restart-btn');
+
+restartBtn.addEventListener('click', () => {
+    localStorage.clear();
+    location.reload();
+});
 
 cartBtn.addEventListener('click', () => {
     let count = 0;
@@ -53,14 +69,24 @@ let betterMinesCount = 0;
 let doubleCoockiesPrice = 100000;
 let doubleCoockiesCount = 0;
 
+let bakerPrice = 500;
+let bakerCount = 0;
+
+let stevePrice = 500;
+let steveCount = 0;
+
 biggerFingerBtn.disabled = true;
 betterMinesBtn.disabled = true;
 doubleCoockiesBtn.disabled = true;
+bakerBtn.disabled = true;
+steveBtn.disabled = true;
 
 function checkPrices(){
     if(goldCount >= biggerFingerPrice){biggerFingerBtn.disabled = false; biggerFingerBtn.style.border = '5px solid #9BEC00'}else{biggerFingerBtn.disabled = true; biggerFingerBtn.style.border = '5px solid #FF0000'};
     if(coockieCount >= betterMinesPrice){betterMinesBtn.disabled = false; betterMinesBtn.style.border = '5px solid #9BEC00'}else{betterMinesBtn.disabled = true; betterMinesBtn.style.border = '5px solid #FF0000'};
     if(goldCount >= doubleCoockiesPrice){doubleCoockiesBtn.disabled = false; doubleCoockiesBtn.style.border = '5px solid #9BEC00'}else{doubleCoockiesBtn.disabled = true; doubleCoockiesBtn.style.border = '5px solid #FF0000'};
+    if(goldCount >= bakerPrice){bakerBtn.disabled = false; bakerBtn.style.border = '5px solid #9BEC00'}else{bakerBtn.disabled = true; bakerBtn.style.border = '5px solid #FF0000'};
+    if(coockieCount >= stevePrice){steveBtn.disabled = false; steveBtn.style.border = '5px solid #9BEC00'}else{steveBtn.disabled = true; steveBtn.style.border = '5px solid #FF0000'};
 };
 
 window.addEventListener('load', () => {
@@ -76,12 +102,16 @@ window.addEventListener('load', () => {
         betterMinesCount = loadData.betterMines.count;
         doubleCoockiesPrice = loadData.doubleCoockies.price;
         doubleCoockiesCount = loadData.doubleCoockies.count;
-        loadCoockiesAndGold();
-    }
+        bakerPrice = loadData.baker.price;
+        bakerCount = loadData.baker.count;
+        stevePrice = loadData.steve.price;
+        steveCount = loadData.steve.count;
+        loadEverything();
+    };
     checkPrices();
 });
 
-function loadCoockiesAndGold(){
+function loadEverything(){
     coockiesP.textContent = `Coockies: ${coockieCount.toLocaleString('de-DE')} +${coockiesToAdd.toLocaleString('de-DE')}`;
     goldP.textContent = `Gold: ${goldCount.toLocaleString('de-DE')} +${goldToAdd.toLocaleString('de-DE')}`;
     biggerFingerPriceP.textContent = `${biggerFingerPrice.toLocaleString('de-DE')}$`;
@@ -90,12 +120,14 @@ function loadCoockiesAndGold(){
     betterMinesCountP.textContent = betterMinesCount;
     doubleCoockiesPriceP.textContent = `${doubleCoockiesPrice.toLocaleString('de-DE')}$`;
     doubleCoockiesCountP.textContent = doubleCoockiesCount;
+    bakerPriceP.textContent = `${bakerPrice.toLocaleString('de-DE')}$`;
+    bakerCountP.textContent = bakerCount;
+    stevePriceP.textContent = `${stevePrice.toLocaleString('de-DE')} Coockies`;
+    steveCountP.textContent = steveCount;
 };
-loadCoockiesAndGold();
+loadEverything();
 
-coockieBtn.addEventListener('click', () => {
-    coockieCount += coockiesToAdd;
-    goldCount += goldToAdd;
+function saveDataToLocalStorage(){
     const data = {
         coockieCount: coockieCount,
         goldCount: goldCount,
@@ -112,10 +144,24 @@ coockieBtn.addEventListener('click', () => {
         doubleCoockies: {
             price: doubleCoockiesPrice,
             count: doubleCoockiesCount
+        },
+        baker: {
+            price: bakerPrice,
+            count: bakerCount
+        },
+        steve: {
+            price: stevePrice,
+            count: steveCount
         }
     };
     localStorage.setItem('data', JSON.stringify(data));
-    loadCoockiesAndGold();
+};
+
+coockieBtn.addEventListener('click', () => {
+    coockieCount += coockiesToAdd;
+    goldCount += goldToAdd;
+    saveDataToLocalStorage();
+    loadEverything();
     checkPrices();
 });
 
@@ -127,8 +173,9 @@ biggerFingerBtn.addEventListener('click', () => {
         biggerFingerPrice = Math.floor(biggerFingerPrice + biggerFingerPrice * 0.2);
         biggerFingerPriceP.textContent = `${biggerFingerPrice.toLocaleString('de-DE')}$`;
         biggerFingerCountP.textContent = biggerFingerCount;
-        loadCoockiesAndGold();
+        loadEverything();
         checkPrices();
+        saveDataToLocalStorage();
     };
 });
 
@@ -140,8 +187,9 @@ betterMinesBtn.addEventListener('click', () => {
         betterMinesPrice = Math.floor(betterMinesPrice + betterMinesPrice * 0.3);
         betterMinesPriceP.textContent = `${betterMinesPrice.toLocaleString('de-DE')} Coockies`;
         betterMinesCountP.textContent = betterMinesCount;
-        loadCoockiesAndGold();
+        loadEverything();
         checkPrices();
+        saveDataToLocalStorage();
     };
 });
 
@@ -153,9 +201,36 @@ doubleCoockiesBtn.addEventListener('click', () => {
         doubleCoockiesPrice = Math.floor(doubleCoockiesPrice + doubleCoockiesPrice * 0.5);
         doubleCoockiesPriceP.textContent = `${doubleCoockiesPrice.toLocaleString('de-DE')}$`;
         doubleCoockiesCountP.textContent = doubleCoockiesCount;
-        loadCoockiesAndGold();
+        loadEverything();
         checkPrices();
+        saveDataToLocalStorage();
     };
 });
 
-// localStorage.clear();
+bakerBtn.addEventListener('click', () => {
+    if(goldCount >= bakerPrice){
+        goldCount -= bakerPrice;
+        coockiesToAdd += 10;
+        bakerCount += 1;
+        bakerPrice = Math.floor(bakerPrice + bakerPrice * 0.3);
+        bakerPriceP.textContent = `${bakerPrice.toLocaleString('de-DE')}$`;
+        bakerCountP.textContent = bakerCount;
+        loadEverything();
+        checkPrices();
+        saveDataToLocalStorage();
+    };
+});
+
+steveBtn.addEventListener('click', () => {
+    if(coockieCount >= stevePrice){
+        coockieCount -= stevePrice;
+        goldToAdd += 10;
+        steveCount += 1;
+        stevePrice = Math.floor(stevePrice + stevePrice * 0.3);
+        steveCountP.textContent = `${stevePrice.toLocaleString('de-De')} Coockies`;
+        steveCountP.textContent = steveCount;
+        loadEverything();
+        checkPrices();
+        saveDataToLocalStorage();
+    };
+});
